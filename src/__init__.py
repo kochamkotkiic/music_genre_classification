667d615fc5bb39b94d3ed14d24afe09f0d990a2b
mikolaj45 <01197795@pw.edu.pl>
2025-12-15 18:33:14 +0100

    adding docker comfiguration

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+################################################################################
+# src/__init__.py
+# Music Genre Classification - Source Code Package
+################################################################################
+
+"""
+Music Genre Classification Project
+
+Moduły:
+  - data_loader: Pobieranie i ładowanie GTZAN dataset
+  - features: Wyodrębnianie cech audio
+  - models: Modele klasyfikacji
+  - evaluation: Metryki oceny
+"""
+
+__version__ = "0.1.0"
+__author__ = "Music Genre Classification Team"
+__all__ = ['data_loader']
